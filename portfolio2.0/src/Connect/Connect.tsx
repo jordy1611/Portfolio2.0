8736df380906c182a1ee200640db9edbf36358c5
Jordan Shryock <jordy1611@gmail.com>
2023-02-10 12:27:53 -0700

    - Setup cname

--- portfolio2.0/src/Connect/Connect.tsx
+++ portfolio2.0/src/Connect/Connect.tsx
@@ -5,7 +5,23 @@ type ConnectProps = {
 function About(props: ConnectProps) {
   return (
     <div className="connect sliding-page">
-      <h1>Connect Page</h1>
+      <div className="connect-info">
+        <h3>Connect</h3>
+        <h2>Sophisticated AF</h2>
+        <p>Whether I'm seeking opportunities or not, I'm always open to making new connections! 
+          <br/>
+          Please reach out if you have any questions or just want to say hi.
+        </p>
+
+      </div>
+      <form>
+        <label>Name</label>
+        <input></input>
+        <label>Email</label>
+        <input></input>
+        <label>Message</label>
+        <textarea></textarea>
+      </form>
     </div>
   );
 }
